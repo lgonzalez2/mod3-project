6a36367c5d0afb2e5a4952d8b69d477b409a4aea
lgonzalez2 <lgonzalez_94@hotmail.com>
2020-09-07 17:31:44 -0500

    created json

--- mod-3-project-backend/app/controllers/favorite_songs_controller.rb
+++ mod-3-project-backend/app/controllers/favorite_songs_controller.rb
@@ -1,11 +1,17 @@
 class FavoriteSongsController < ApplicationController
 
     def index
-
+        favorite_songs = FavoriteSong.all
+        render json: favorite_songs, only: [:id, :title, :artist, :video_url, :likes], include: [:user, :comments]
     end
 
     def show
-    
+        song = FavoriteSong.find_by(id: params[:id])
+        if song
+            render json: song.slice(:id, :title, :artist, :video_url, :likes, :user, :comments)
+        else 
+            render json: { message: 'No song found with that id' }
+        end 
     end
 
 end
